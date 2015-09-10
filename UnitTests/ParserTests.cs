fb0afdc3460859a5cace8ebd53fa318eccb93fef
vmax44 <vmax44@tut.by>
2015-09-11 02:45:04 +0300

    1

--- UnitTests/ParserTests.cs
+++ UnitTests/ParserTests.cs
@@ -12,15 +12,6 @@ namespace UnitTests
         [TestMethod]
         public void SummParseTest()
         {
-            Parser parser=new ParserWatinExist();
-            decimal expected=293839.49m;
-            string s1 = "f293 839.49r.";
-            //string s2 = "293,839.49r.";
-            string s3 = "293 839,49rub";
-
-            //Assert.AreEqual(expected, parser.summParse(s1));
-            //Assert.AreEqual(expected, parser.summParse(s2));
-            //Assert.AreEqual(expected, parser.summParse(s3));
 
         }
     }
